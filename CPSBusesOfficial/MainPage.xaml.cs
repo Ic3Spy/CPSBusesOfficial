f7c271faf30f15809671856db0c3f9643558ea02
Ic3Spy <rhenz11sulit@gmail.com>
2025-04-01 13:15:22 +0800

    Add project files.

---
+++ CPSBusesOfficial/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CPSBusesOfficial
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
